2b2f1c17a740814704a4967fce1ac454728be008
dkeysil <dkeysil@protonmail.com>
2020-12-11 02:50:39 +0300

    Добавил методы, добавил описания

--- __main__.py
+++ __main__.py
@@ -1,6 +1,46 @@
-from aiohttp import web
+from aiohttp import web, ClientSession
+from xml.dom import minidom
+import json
 
 routes = web.RouteTableDef()
+currency_lst = minidom.parse('currency_list.asp')
+
+
+def currency_dict(doc: minidom.Document) -> dict:
+    """
+    Обрабатывает XML документ
+    :param doc:
+    :return:
+    """
+    items = doc.firstChild.getElementsByTagName('Item')
+    dct = {}
+
+    for item in items:
+        try:
+            _id = item.attributes['ID'].value
+            name = item.getElementsByTagName('Name')[0].firstChild.data
+            symbol = item.getElementsByTagName('ISO_Char_Code')[0].firstChild.data
+            dct.update({symbol: (name, _id)})
+        except AttributeError:
+            pass
+
+    return dct
+
+
+def currency_list(curr_dct: dict) -> list:
+    lst = []
+    for key in curr_dct.keys():
+        lst.append([key, curr_dct[key][0]])
+
+    return lst
+
+
+currency_dct = currency_dict(currency_lst)
+currency_lst = currency_list(currency_dct)
+
+
+def custom_json_dumps(obj):
+    return json.dumps(obj, ensure_ascii=False)
 
 
 @routes.get('/api/currency_list')
@@ -9,7 +49,24 @@ async def get_currency_list(request: web.Request):
     :param request:
     :return: Список валют, который содержит все доступные валюты в формате [('RUB', 'Рубль'), ..]
     """
-    return web.json_response()
+
+    return web.json_response(currency_lst, dumps=custom_json_dumps, status=200)
+
+
+def currency_processing(doc: minidom.Document) -> (float, float, float):
+    """
+    Обрабатывает полученные данные
+    :param doc:
+    :return: Стоимость для первой даты, стоимость для второй даты, разница между второй и первой
+    """
+    first = doc.firstChild.firstChild.getElementsByTagName('Value')[0].firstChild.data
+    last = doc.lastChild.lastChild.getElementsByTagName('Value')[0].firstChild.data
+    first = float(first.replace(',', '.'))
+    last = float(last.replace(',', '.'))
+    # Деньги надо обрабатывать в минимальной единице (копейки), а не переводить во float, но я этого тут не делал,
+    # так как сайт ЦБ предоставляет и так округленные данные
+
+    return first, last, (last - first)
 
 
 @routes.get('/api/exchange_rate_difference')
@@ -17,10 +74,46 @@ async def get_exchange_rate_difference(request: web.Request):
     """
     Возвращает разницу курса относительно рубля между двумя датами за выбранную дату
     GET параметры: символьный код продукта, дата 1, дата 2
+    Например: api/exchange_rate_difference?symb=BYN&date_req1=12/07/2020&date_req2=15/07/2020
     :param request:
     :return: Курс за первую дату, курс за вторую дату и разницу между ними
     """
-    return web.json_response()
+    args = request.query
+    try:
+        title = currency_dct.get(args.get("symb"))[0]
+        currency_id = currency_dct.get(args.get("symb"))[1]
+    except TypeError:
+        return web.json_response({'error': 'Exchange rate not found'}, status=404)
+    exchange_api_link = 'http://www.cbr.ru/scripts/XML_dynamic.asp?' \
+                        f'date_req1={args.get("date_req1")}&' \
+                        f'date_req2={args.get("date_req2")}&' \
+                        f'VAL_NM_RQ={currency_id}'
+
+    async with ClientSession() as session:
+        async with session.get(exchange_api_link) as resp:
+            # Get exchange rates
+            result = await resp.text()
+
+    doc = minidom.parseString(result)
+    try:
+        val_curs = doc.firstChild.firstChild.data
+        if val_curs == "Error in parameters":
+            return web.json_response({'error': 'Error in parameters'}, status=422)
+    except AttributeError:
+        pass
+
+    try:
+        first, last, difference = currency_processing(doc)
+        jsn = {
+            'title': title,
+            'first_exchange_rate': first,
+            'second_exchange_rate': last,
+            'difference': difference
+        }
+    except AttributeError:
+        return web.json_response({'error': 'Error in parameters'}, status=422)
+
+    return web.json_response(jsn, dumps=custom_json_dumps, status=200)
 
 
 app = web.Application()
